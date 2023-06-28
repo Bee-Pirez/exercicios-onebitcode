67091b9c5ed2f515549ddba14b01afaa69a215d8
Beatriz <beatrizcarmoperez@gmail.com>
2023-06-28 15:51:18 -0300

    add exercise 7

---
+++ programming-logic-js/exercicio-7-fila-espera/consultorio.js
@@ -0,0 +1,39 @@
+let fila = []
+let opcao = ""
+
+do {
+  let pacientes = ""
+  for ( let i = 0; i < fila.length; i++){
+    pacientes += (i + 1) + "°" + "" + fila[i] + "\n"
+  }
+
+  opcao = prompt(
+    "Pacientes em espera: \n" + pacientes +
+    "\n- O que deseja fazer:\n1.Adicionar novo paciente\n2.Consultar paciente\n3.Pesquisar posição do paciente \n4.Sair\n"
+  )
+
+  switch (opcao) {
+    case "1": 
+      const addPaciente = prompt("Qual é o nome do paciente que deseja adicionar?")
+      fila.push(addPaciente)
+      break
+    case "2":
+      const pacienteConsultado = fila.shift()
+      alert ("O(a) paciente " + pacienteConsultado + " Foi consultado(a), portanto foi removido da fila de espera.")
+      break
+    case "3":
+      const nomePaciente = prompt("Digite o nome do paciente que deseja pesquisar:");
+      const posicao = fila.indexOf(nomePaciente);
+      if (posicao !== -1) {
+        alert("O(a) paciente " + nomePaciente + " está na posição " + (posicao + 1) + " da fila de espera.");
+      } else {
+        alert("O(a) paciente " + nomePaciente + " não foi encontrado(a) na fila de espera.");
+      }
+      break;
+    case "4":
+      alert("Encerrando fila de espera...")
+    default:
+      alert("Opção inválida!. Escolha uma opção váilda da próxima vez.")
+      break
+  }
+}while (opcao !== "4")
