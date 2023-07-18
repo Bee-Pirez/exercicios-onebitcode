adcfbb7d89b41b9647b606b9188f9a2d86923198
Beatriz <beatrizcarmoperez@gmail.com>
2023-07-18 17:07:38 -0300

    add function calculate

--- DOM/calculadora/index.js
+++ DOM/calculadora/index.js
@@ -33,3 +33,13 @@ input.addEventListener("keydown", function (ev) {
     calculate()
   }
 })
+
+document.getElementById("equal").addEventListener("click", calculate)
+
+function calculate() {
+  resultInput.value = "ERROR"
+  resultInput.classList.add("error")
+  const result = eval(input.value)
+  resultInput.value = result
+  resultInput.classList.remove("error")
+}
