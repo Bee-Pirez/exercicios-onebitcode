e76eaa50ea7b80da979af6c0821b8cb1b6fdfd2d
Beatriz <beatrizcarmoperez@gmail.com>
2023-07-18 16:53:49 -0300

    event keydown

--- DOM/calculadora/index.js
+++ DOM/calculadora/index.js
@@ -19,3 +19,17 @@ document.getElementById("clear").addEventListener("click", function () {
   resultInput.value = ""
   resultInput.classList.remove("error")
 })
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault()
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key
+    return
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1)
+  }
+  if (ev.key === "Enter") {
+    calculate()
+  }
+})
