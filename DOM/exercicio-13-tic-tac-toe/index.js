2453008b24c70ee47a754e51c6d79191b02b702c
Beatriz <beatrizcarmoperez@gmail.com>
2023-07-25 22:08:12 -0300

    Feat: add function to update current player

--- DOM/exercicio-13-tic-tac-toe/index.js
+++ DOM/exercicio-13-tic-tac-toe/index.js
@@ -3,4 +3,8 @@ const boardRegions = document.querySelectorAll('#gameBoard span')
 let vBoard = []
 let turnPlayer = ''
 
-
+// Função para atualizar o título do jogador atual na interface
+function updateTitle() {
+  const playerInput = document.getElementById(turnPlayer);
+  document.getElementById('turnPlayer').innerText = playerInput.value;
+}
