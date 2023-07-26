82422c7e4c39525b51481b8d437291e4f9299e8b
Beatriz <beatrizcarmoperez@gmail.com>
2023-07-26 10:48:40 -0300

    Feat: adding functions to check winning regions on the board

--- DOM/exercicio-13-tic-tac-toe/index.js
+++ DOM/exercicio-13-tic-tac-toe/index.js
@@ -27,3 +27,41 @@ function initializeGame() {
     element.addEventListener('click', handleBoardClick);
   });
 }
+
+// Função auxiliar para verificar se uma combinação de células representa uma vitória
+function checkWin(cells) {
+  if (cells[0] && cells[0] === cells[1] && cells[0] === cells[2]) {
+    return true;
+  }
+  return false;
+}
+
+// Obtém as regiões vencedoras no tabuleiro
+function getWinRegions() {
+  const winRegions = [];
+  
+  // Verifica as linhas
+  for (let i = 0; i < 3; i++) {
+    if (checkWin(vBoard[i])) {
+      winRegions.push(i + '.0', i + '.1', i + '.2');
+      break;
+    }
+  }
+
+  // Verifica as colunas
+  for (let i = 0; i < 3; i++) {
+    if (checkWin([vBoard[0][i], vBoard[1][i], vBoard[2][i]])) {
+      winRegions.push('0.' + i, '1.' + i, '2.' + i);
+      break;
+    }
+  }
+
+  // Verifica as diagonais
+  if (checkWin([vBoard[0][0], vBoard[1][1], vBoard[2][2]])) {
+    winRegions.push('0.0', '1.1', '2.2');
+  } else if (checkWin([vBoard[0][2], vBoard[1][1], vBoard[2][0]])) {
+    winRegions.push('0.2', '1.1', '2.0');
+  }
+
+  return winRegions;
+}
