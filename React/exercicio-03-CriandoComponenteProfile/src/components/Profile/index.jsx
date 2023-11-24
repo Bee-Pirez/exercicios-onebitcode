3e140f67127a0a8baf07af0ca518bdb59013e797
Beatriz <beatrizcarmoperez@gmail.com>
2023-11-24 20:13:09 -0300

    ajustes

--- React/exercicio-03-CriandoComponenteProfile/src/components/Profile/index.jsx
+++ React/exercicio-03-CriandoComponenteProfile/src/components/Profile/index.jsx
@@ -1,3 +1,5 @@
+import LinkButton from "../LinkButton"
+import ProfileSection from "./ProfileSection"
 import styles from "./styles.module.css"
 
 export default function Profile(props) {
@@ -5,13 +7,15 @@ export default function Profile(props) {
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
       <h2 className={styles.name}>{props.name}</h2>
-      <div>{props.bio}</div>
-      <div>{props.email}</div>
-      <div className={styles.links}>
-        <a href={props.githubUrl} target="_blank">GitHub</a>
-        <a href={props.linkedinUrl} target="_blank">LinkedIn</a>
-        <a href={props.behanceUrl} target="_blank">Twitter</a>
-      </div>
+      <ProfileSection>{props.bio}</ProfileSection>
+      <ProfileSection>{props.email}</ProfileSection>
+      <ProfileSection>
+        <div className={styles.links}>
+          <LinkButton href={props.githubUrl} target="_blank">GitHub</LinkButton>
+          <LinkButton href={props.linkedinUrl} target="_blank">LinkedIn</LinkButton>
+          <LinkButton href={props.behanceUrl} target="_blank">Twitter</LinkButton>
+        </div>
+      </ProfileSection>
     </div>
   )
 }
