f570443098bd5c5964c046232a0b59296492a153
Beatriz <beatrizcarmoperez@gmail.com>
2023-11-27 22:45:47 -0300

    props com rest params

--- React/exercicio-03-CriandoComponenteProfile/src/components/Profile/ProfileSection/index.jsx
+++ React/exercicio-03-CriandoComponenteProfile/src/components/Profile/ProfileSection/index.jsx
@@ -1,8 +1,13 @@
-import styless from "./styles.module.css"
+import styles from "./styles.module.css"
 
 export default function ProfileSection (props) {
   return (
-    <div className={styless.wrapper}>
+    <div 
+      //pegando todas as props e colocando direto nas divs
+      {...props}
+      //template literal - dentro de string - acrescentar classes extras no css modules
+      className={`${styles.wrapper} ${props.className}`}
+    >
       {props.children}
     </div>
   )
--- React/exercicio-03-CriandoComponenteProfile/src/components/Profile/index.jsx
+++ React/exercicio-03-CriandoComponenteProfile/src/components/Profile/index.jsx
@@ -1,4 +1,5 @@
 import LinkButton from "../LinkButton"
+import Title from "../Title"
 import ProfileSection from "./ProfileSection"
 import styles from "./styles.module.css"
 
@@ -6,15 +7,22 @@ export default function Profile(props) {
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
-      <h2 className={styles.name}>{props.name}</h2>
+      <Title>
+        <span>Name: </span>
+        {props.name}
+        <button>Follow</button>
+      </Title>
       <ProfileSection>{props.bio}</ProfileSection>
       <ProfileSection>{props.email}</ProfileSection>
-      <ProfileSection>
-        <div className={styles.links}>
-          <LinkButton href={props.githubUrl} target="_blank">GitHub</LinkButton>
-          <LinkButton href={props.linkedinUrl} target="_blank">LinkedIn</LinkButton>
-          <LinkButton href={props.behanceUrl} target="_blank">Twitter</LinkButton>
-        </div>
+      <ProfileSection
+         className={styles.links}
+         id="links-section"
+         data-test="some value"
+         aria-label="social links"
+      >
+        <LinkButton href={props.githubUrl} target="_blank">GitHub</LinkButton>
+        <LinkButton href={props.linkedinUrl} target="_blank">LinkedIn</LinkButton>
+        <LinkButton href={props.behanceUrl} target="_blank">Twitter</LinkButton>
       </ProfileSection>
     </div>
   )
